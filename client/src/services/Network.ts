e035628ba9c313ca2ec72e21ea4ac766d8509423
oneair <gilberterwan@gmail.com>
2026-02-28 21:44:38 +0100

    real adress

--- client/src/services/Network.ts
+++ client/src/services/Network.ts
@@ -23,8 +23,7 @@ export class Network {
   sessionId!: string;
 
   constructor() {
-    // const endpoint = import.meta.env.VITE_COLYSEUS_URL ?? `ws://${window.location.hostname}:2567`;
-    const endpoint = `https://api.erwangilbert.com`;
+    const endpoint = import.meta.env.VITE_COLYSEUS_URL ?? `ws://${window.location.hostname}:2567`;
     this.client = new Client(endpoint);
     this.joinLobbyRoom().then(() => {
       // TODO : store in local store
